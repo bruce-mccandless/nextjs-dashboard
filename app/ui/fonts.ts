dd5df86cbecfea69ce6138d705774112aa30f0d2
Lego Blocks <lego.elite.awesomeness@gmail.com>
2024-05-11 23:41:06 -0400

    ?

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,19 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Red_Hat_Display, Roboto, Ubuntu } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
   });
+export const roboto = Roboto({
+    weight: ['400', '500', '700'],
+    subsets: ['latin'],
+  });
+export const ubuntu = Ubuntu({
+    weight: ['400', '500'],
+    subsets: ['latin'],
+  });
+export const redhat = Red_Hat_Display({
+  weight:['300', '400'],
+  subsets: ['latin'],
+})
